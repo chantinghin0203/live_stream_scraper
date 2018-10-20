a5e6058b0462efbfda1fc2b8e234863eff2350ab
chantinghin0203 <chantinghin0203@gmail.com>
2018-10-21 00:07:32 +0800

    first

--- server/controller/football_livestream.py
+++ server/controller/football_livestream.py
@@ -1,10 +1,34 @@
+from urllib.request import urlopen
+import time
+from bs4 import BeautifulSoup
 from flask_restplus import Resource, Namespace
 
-# api = Blueprint("Index", __name__, url_prefix="/football")
 ns_football = Namespace("football")
 
 
-@ns_football.route('/get_user_info')
+@ns_football.route('/get-stream-list')
 class Football(Resource):
+    URL = 'https://www.reddit.com/r/soccerstreams/'
+    REDDIT_URL = 'https://www.reddit.com'
+
     def get(self):
-        return "football works"
+        stream_links = []
+        soup = self.get_data_from_reddit()
+
+        list_title = soup.find_all("a", class_="SQnoC3ObvgnGjWt90zD9Z")
+        for title in list_title:
+            title_text = title.find("h2").text
+            if "vs" in title_text:
+                stream_links.append(f"{title_text} : {Football.REDDIT_URL}{title.get_attribute_list('href')[0]}")
+
+        return stream_links
+
+    def get_data_from_reddit(self):
+        for i in range(20):
+            try:
+                soup = BeautifulSoup(urlopen(Football.URL), "html.parser")
+                break
+            except Exception as e:
+                print(e)
+                time.sleep(1)
+        return soup
