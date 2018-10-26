a07f057431f56de061e78c4cc30818e74e26a499
chantinghin0203 <chantinghin0203@gmail.com>
2018-10-27 02:25:57 +0800

    fixed deploy url

--- server/controller/FrontEndController.py
+++ server/controller/FrontEndController.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, Blueprint
 blueprint_ui = Blueprint("ui", __name__,url_prefix="/ui")
 
 
+
 @blueprint_ui.route("/")
 def ui():
+    return render_template("index.html")
+
+@blueprint_ui.errorhandler(404)
+def not_found():
     return render_template("index.html")
