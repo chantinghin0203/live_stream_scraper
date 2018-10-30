96112aebb51aac40496ab62e1a1b3f91d18a851e
chantinghin0203 <chantinghin0203@gmail.com>
2018-10-30 20:37:47 +0800

    with changed ui path to index

--- server/controller/FrontEndController.py
+++ server/controller/FrontEndController.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, Blueprint
 
-blueprint_ui = Blueprint("ui", __name__, url_prefix="/ui")
+blueprint_ui = Blueprint("ui", __name__, url_prefix="/")
 
 
 @blueprint_ui.route("/", defaults={"path": ""})
